f2bf2daefc1b3a7ba7b06fa41c161736a4038fab
Naimur53 <naimurrhman53@gmail.com>
2021-03-29 23:56:20 +0600

    done

---
+++ elemants/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+     
+    $('.owl-carousel').owlCarousel({
+        loop:true, 
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+
+});
