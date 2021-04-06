5b68c2f3d67dcec0e86fe90394e9d6d80bcc0bfe
Naimur53 <naimurrhman53@gmail.com>
2021-04-06 15:17:03 +0600

    slider done

--- elemants/js/main.js
+++ elemants/js/main.js
@@ -15,7 +15,27 @@ $(document).ready(function(){
             }
         }
     })
-
-
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 1,
+        cssMode: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+          loop: true,
+          loopedSlides: 4,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        mousewheel: true,
+        keyboard: true,
+      });
+    
 
 });
