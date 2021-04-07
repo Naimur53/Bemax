fd793d57cf4e46df722369eb31554ccbcd5120b4
Naimur53 <naimurrhman53@gmail.com>
2021-04-07 20:48:57 +0600

    doen|

--- elemants/js/main.js
+++ elemants/js/main.js
@@ -57,5 +57,21 @@ $(document).ready(function(){
             }
         }
     })
-
+     
+    $('.brand-slider').owlCarousel({
+      loop:true, 
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:6
+          }
+      }
+  })
+ 
 });
